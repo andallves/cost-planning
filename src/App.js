4743b28beab63f1a543fe3afae0444f4fad3f424
Andre Alves <37510133+andallves@users.noreply.github.com>
2023-02-10 21:51:04 -0300

    Home page :up:

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Container from "./components/layout/Container";
+import Footer from "./components/layout/Footer";
+import Navbar from "./components/layout/Navbar";
 import About from "./components/pages/About";
 import Contact from "./components/pages/Contact";
 import Home from "./components/pages/Home";
 import NewProject from "./components/pages/NewProject";
+import Projects from "./components/pages/Projects";
 
 function App() {
   return (
     <Router>
-      <ul>
-        <li><Link to={"/"}>Home</Link></li>
-        <li><Link to={"/about"}>About</Link></li>
-        <li><Link to={"/contact"}>Contact</Link></li>
-        <li><Link to={"/newproject"}>New Project</Link></li>
-      </ul>
+      <Navbar />
 
       <Container custumClass="min-height">
         <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/about" element={<About/>}/>
-          <Route path="/contact" element={<Contact/>}/>
-          <Route path="/newproject" element={<NewProject/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About/> } />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/newproject" element={<NewProject />} />
+          <Route path="/projects" element={<Projects />} />
         </Routes>
       </Container>
-      <p>Footer</p>
+
+      <Footer />
     </Router>
   );
 }
